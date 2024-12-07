77cd110c0cd39245ccaad937d4cec620a957520a
Vaibhav Singh <vaibhavsinghr.08@gmail.com>
2024-12-07 13:45:24 +0530

    fix configs

--- searchProject/build.sh
+++ searchProject/build.sh
@@ -4,8 +4,5 @@ set -o errexit
 # Install dependencies first
 pip install -r requirements.txt
 
-# Change to the project directory
-cd searchProject
-
 # Collect static files
 python manage.py collectstatic --no-input
